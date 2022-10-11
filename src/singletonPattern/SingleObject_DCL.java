3f4f9da2c95bf88d708d98c6d4b48a229d243f89
ckl <1051928078@qq.com>
2022-10-11 13:37:10 +0800

    单例模式

---
+++ src/singletonPattern/SingleObject_DCL.java
@@ -0,0 +1,24 @@
+package singletonPattern;
+
+public class SingleObject_DCL {
+    private static volatile SingleObject_DCL instance;
+
+    private SingleObject_DCL() {
+
+    }
+
+    public static SingleObject_DCL getInstance() {
+        if (instance == null) {
+            synchronized (SingleObject_DCL.class) {
+                if (instance == null) {
+                    instance = new SingleObject_DCL();
+                }
+            }
+        }
+        return instance;
+    }
+
+    public void showMessage() {
+        System.out.println("双重锁校验单例");
+    }
+}
